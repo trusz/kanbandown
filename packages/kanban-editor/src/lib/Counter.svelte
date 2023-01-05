69085cccb55bd070b35cdc018d32641ed74954e5
Tamás Rusz <t.rusz@sprinteins.com>
2023-01-05 16:25:28 +0100

    feat: communication between fe and be

--- packages/kanban-editor/src/lib/Counter.svelte
+++ packages/kanban-editor/src/lib/Counter.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+	
+	// @ts-ignore
+	const vscode = acquireVsCodeApi();
+	
+	let count: number = 0
+	const increment = () => {
+		count += 1
+		vscode.postMessage({
+			type: "inc"
+		})
+	}
 </script>
 
 <button on:click={increment}>
-  count is {count}
+	count is {count}
 </button>
---
+++ todo.knb.md
@@ -0,0 +1,14 @@
+KanbanDown
+==========
+
+
+Todo
+----
+
+- [ ] extract vscode-svelte-plugin starter template to another report
+- [ ] checkout dasyui
+- [ ] move a real project place
+- [ ] roadmap, planned features
+- [ ] markdown parsing and rendering: md -> object, object->md, md->html
+- [ ] component library
+- [ ] vscode marketplace
